d9e6f75423385f2c0d7faec3c583f8670d4fd009
zh-987 <zhasulanasainov@gmail.com>
2022-11-21 22:01:39 +0600

    Web Api Book Store

---
+++ WebApplication5/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication5.Models;
+using System.Data;
+using System.Data.Entity;
+
+namespace WebApplication5.Controllers
+{
+
+    public class ValuesController : ApiController
+    {
+        BookContext dbbook = new BookContext(); 
+        // GET api/values
+        public IEnumerable<Book> GetBooks()
+        {
+            return dbbook.book;
+        }
+
+        // GET api/values/5
+        public Book GetBook(int id)
+        {
+            Book bookID = dbbook.book.Find(id);
+            return bookID;
+        }
+
+        [HttpPost]
+        // POST api/values
+        public void CreatePost([FromBody] Book postbook)
+        {
+            dbbook.book.Add(postbook);
+            dbbook.SaveChanges();
+        }
+
+        [HttpPut]
+        // PUT api/values/5
+        public void EditBook(int id, [FromBody] Book editbook)
+        {
+            if(id == editbook.id)
+            {
+                dbbook.Entry(editbook).State = EntityState.Modified;
+                dbbook.SaveChanges();
+            }
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            Book deletebook = dbbook.book.Find(id);
+            if (deletebook != null)
+            {
+                dbbook.book.Remove(deletebook);
+                dbbook.SaveChanges();
+            }
+        }
+    }
+}
---
+++ WebApplication5/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string author { get; set; }
+        public int releasedate { get; set; }
+    }
+
+    public class BookContext : DbContext
+    {
+        public DbSet <Book> book { get; set; }
+    }
+}
